aa9313eafd1974bb0e0bffa7071c0bc31e01690b
QuinnDur <71420962+QuinnDur@users.noreply.github.com>
2020-10-14 20:57:30 -0700

    Finished Game!

--- Game-Project/3-conditional-statement/sketch.js
+++ Game-Project/3-conditional-statement/sketch.js
@@ -1,4 +1,3 @@
-
 let boats = [];
 
 let waves =[];
@@ -7,18 +6,22 @@ let me;
 
 let mySound;
 
-let hits = 0
+let hits = 0;
 
 let img;
 
 let wave1;
+
+let jumping = false;
+
+let ySpeed = 3;
 
 function preload() {
   soundFormats('mp3', 'ogg', 'wav');
   mySound = loadSound('impact.wav')
   boat = loadAnimation('boat/Boat0001.png','boat/Boat0004.png')
   img = loadImage('BackImage.jpeg')
-  wave = loadAnimation('waveSprite/Wave1.png','waveSprite/Wave1.png');
+  wave = loadAnimation('waveSprite/Wave1.png','waveSprite/Wave2.png');
   fisherman = loadAnimation('fishermanSprite/FishermanWalk1.png', 'fishermanSprite/FishermanWalk4.png');
 
 
@@ -26,55 +29,59 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(500, 400);
+  createCanvas(800, 400);
 
   //make one avatar called me
   me = new Avatar(width/2, 300, 3);
 
-  wave1 = new Wave(680, 250, 2);
-
+  wave1 = new Wave(1000, 250, 2);
+  boat1 = new Boat(1000, 300, -2);
 }
 
 function draw(){
-	background(img,0,0,500,400);
+  background(img,0,0,800,400);
 
   noStroke();
-   fill(3,96,167);
-   rect(0,311,500,89);
+  fill(3,96,167);
+  rect(0,311,800,89);
 
 
-   wave1.drawWave();
-   wave1.moveWave();
-   wave1.hitWave();
+  wave1.drawWave();
+  wave1.moveWave();
+  wave1.hitWave();
 
-   if (frameCount % 300 == 0) {
-       let  b = new Wave(680, 250, 2);
-       waves.push(b);
-       console.log(waves); //print the balls array to the console
-     }
+
+  if (frameCount % 300 == 0) {
+      let  b = new Wave(1000, 250, 2);
+      waves.push(b);
+      console.log(waves); //print the balls array to the console
+    }
 
  // draw all the balls in that array
  for (let i = 0; i < waves.length; i++) {
-      waves[i].drawWave();
-          waves[i].moveWave();
-           waves[i].hitWave();
+   waves[i].drawWave();
+   waves[i].moveWave();
+   waves[i].hitWave();
+}
+   boat1.drawBoat();
+   boat1.moveBoat();
+   boat1.hitBoat();
 
-  me.drawMe();
-  me.moveMe();
-
-  if (frameCount % 50 == 0) {
-      let  b = new Boat(width, 200, -2);
+  if (frameCount % 700 == 0) {
+      let  b = new Boat(1000, 300, -2);
       boats.push(b);
       console.log(boats); //print the balls array to the console
     }
 
-//	draw all the balls in that array
-	for (let i = 0; i < boats.length; i++) {
-	 	      boats[i].drawBoat();
-       	  boats[i].moveBoat();
-        	boats[i].bounceBoat();
-	  }
-
+//      draw all the balls in that array
+  for (let i = 0; i < boats.length; i++) {
+    boats[i].drawBoat();
+    boats[i].moveBoat();
+    boats[i].hitBoat();
+  }
+  me.drawMe();
+  me.moveMe();
+  me.die();
 }
 
 //avatar class
@@ -101,11 +108,25 @@ drawMe(){  // draw the running person
 }
 
 
-	moveMe(){
+        moveMe(){
     if (keyIsDown(32)) {
-      me.vel.y=-3
-       //When spacebar is pressed, character moves upward at speed of 50 frames per second, increasing by 10 each time. Once they hit a height of 150, they drop by 55 units per second until they hit the ground
+      jumping = true;
+      //me.speed.y=-3
     }
+    if (jumping == true){
+      let maxHeight = 50;
+      this.y = this.y - ySpeed;
+      if (this.y <= maxHeight){
+        ySpeed = -1 * ySpeed;
+      }
+      if (this.y >= 300){
+        jumping = false;
+        ySpeed = -1 * ySpeed;
+      }
+    }
+//must code fuction up and coming down
+//When spacebar is pressed, character moves upward at speed of 50 frames per second, increasing by 10 each time. Once they hit a height of 150, they drop by 55 units per second until they hit the ground
+
 
     if (keyIsDown(LEFT_ARROW)) {
         this.x -= this.speed;
@@ -133,14 +154,18 @@ drawMe(){  // draw the running person
    fill("black");
    stroke("black");
    if (hits >= 3) {
-     text("Game Over", 180, 100);
+     fill(252, 3, 3)
+     text("Game Over", 330, 100);
      //exit();
    } else if (hits == 2) {
-     text("Lives: 1", 180, 100);
+     fill(11, 252, 3)
+     text("Lives: 1", 330, 100);
    } else if (hits == 1) {
-     text("Lives: 2", 180, 100);
+     fill(11, 252, 3)
+     text("Lives: 2", 330, 100);
    } else if (hits == 0) {
-     text("Lives: 3", 180, 100);
+     fill(11, 252, 3)
+     text("Lives: 3", 330, 100);
    }
   }
 
@@ -166,39 +191,51 @@ class Wave {
         if (frameCount % 50 == 0) {
           hits = hits + 1
         }
-
+}
+}
+}
 //ball class from which to create new balls with similar properties.
 class Boat{
 
-	//every ball needs an x value, a y value, and a speed
-	constructor(x,y, speed){
-		this.x = x;
+        //every ball needs an x value, a y value, and a speed
+    constructor(x,y, speed){
+    this.x = x;
     this.y = y;
     this.speed = speed/2;
-	}
+        }
 
-	// draw a ball on the screen at x,y
-	drawBoat(){
-    	stroke(0);
-      strokeWeight(1);
-animation(boat, this.x/10, this.y/10 )
-	}
+        // draw a ball on the screen at x,y
+    drawBoat(){
+      //stroke(0);
+      //strokeWeight(1);
+      animation(boat, this.x, this.y)
+    }
 
-	//update the location of the ball, so it moves across the screen
-	moveBoat(){
-		this.x = this.x+ this.speed;
-		this.y = this.y+.5;
-	}
+        //update the location of the ball, so it moves across the screen
+        moveBoat(){
+                this.x = this.x+ this.speed;
+                //this.y = this.y+.5;
+        }
 
-	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
-  	bounceBoat(){
-    		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
-            this.speed = -this.speed;
+        //if the ball hits the person, change the speed value to negative (send it in the opposite direction)
+        bounceBoat(){
+            //if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
+            //this.speed = -this.speed;
             fill("red")
             ellipse(this.x, this.y, 30,30)
-            mySound.setVolume(0.1);
-            mySound.play();
-    		}
-  	}
+          //  mySound.setVolume(0.1);
+          //  mySound.play();
+                }
+        hitBoat(){
+          if (this.x >= me.x-150 && this.x <= me.x+150 && this.y > me.y-100 && this.y < me.y+100){
+            fill("red")
+            ellipse(this.x, this.y, 30,30)
+              //me.x = width/2
+              //me.y = 300
+              if (frameCount % 300 == 0) {
+                hits++
+              }
+        }
 
+  }
 }
